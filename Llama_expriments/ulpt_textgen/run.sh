d25db907f8bd94ec5342203102e1c40b944a1798
ULPT-anonymous <144285180+ULPT-anonymous@users.noreply.github.com>
2025-05-06 10:05:01 -0600

    Update run.sh

--- Llama_expriments/ulpt_textgen/run.sh
+++ Llama_expriments/ulpt_textgen/run.sh
@@ -11,7 +11,7 @@ do
   echo "---------------------------------" >> "$LOG_FILE"
   echo "Running experiment with rank: $r" >> "$LOG_FILE"
   # Run the Python script with the current learning rate; adjust dataset or other parameters as needed
-  CUDA_VISIBLE_DEVICES=0 python main.py --lr "2e-1" --dataset "gsm8k" --model_name "3b" --m 10 --bs 2 --acc_step 2 --epochs 3 --r "$r" 2>&1 | tee >(grep "Final Answer Accuracy" >> "$LOG_FILE")
+  CUDA_VISIBLE_DEVICES=0 python main.py --lr "1e-2" --dataset "gsm8k" --model_name "3b" --m 10 --bs 2 --acc_step 2 --epochs 3 --r "$r" 2>&1 | tee >(grep "Final Answer Accuracy" >> "$LOG_FILE")
   echo "Completed experiment with rank: $r" >> "$LOG_FILE"
 done
 
